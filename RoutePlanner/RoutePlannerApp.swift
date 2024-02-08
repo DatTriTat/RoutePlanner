0f9a7c7987532de88d00b4de8c30ffec0b3ae9a0
Khanh Chung <khanh.d.chung@gmail.com>
2024-02-08 11:00:07 -0800

    Initial Commit

---
+++ RoutePlanner/RoutePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutePlannerApp.swift
+//  RoutePlanner
+//
+//  Created by Khanh Chung on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RoutePlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
